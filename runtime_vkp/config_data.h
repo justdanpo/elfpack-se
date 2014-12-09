f747022b2b87dc43e589bb28f2fb40fb699ede07
Eugene <IronMaster@users.noreply.github.com>
2014-12-09 19:05:05 +0000

    ! sync

--- runtime_vkp/config_data.h
+++ runtime_vkp/config_data.h
@@ -7,6 +7,8 @@ extern const char APP_SIZE_str[9];
 extern const char PCORE_TO_PATCH1_str[9];
 extern const char PCORE_TO_PATCH2_str[9];
 extern const char PCORE_TO_PATCH3_str[9];
+extern const char PCORE_TO_PATCH4_str[9];
+extern const char PCORE_TO_PATCH5_str[9];
 
 
 #endif
--- runtime_vkp/patch_pcore.asm
+++ runtime_vkp/patch_pcore.asm
@@ -8,9 +8,22 @@ patch_pcore_static_cache:
 	MOV	R0, R4
 	MOV	R1, R7
 	BL	check_static_after_map
-	LDR	R6, _pcore_return_addr
+	LDR	R6, _patch_pcore_static_cache_return_addr
 	BX	R6
 	
-_pcore_return_addr DCD 0x12345678
+_patch_pcore_static_cache_return_addr DCD 0x12345678
+
+
+	extern	check_nonstatic_after_map
+
+	public patch_pcore_nonstatic_cache
+patch_pcore_nonstatic_cache:
+	ADD	R1, R12, R0, LSL #10
+	MOV	R0, R5
+	BL	check_nonstatic_after_map
+	LDR	R1, _patch_pcore_nonstatic_cache_return_addr
+	BX	R1
+	
+_patch_pcore_nonstatic_cache_return_addr DCD 0x87654321
 	
 	end
