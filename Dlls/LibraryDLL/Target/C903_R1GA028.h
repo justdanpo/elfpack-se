0b8da6707018601394247ba4c718b74f1bffcae9
farid1991 <farid.irwan.1991@gmail.com>
2024-01-04 17:28:56 +0700

    Elfpack +C903; +T650; +T715; +Z710;

---
+++ Dlls/LibraryDLL/Target/C903_R1GA028.h
@@ -0,0 +1,41 @@
+#define C903_R1GA028
+#define A2
+#define DB3210
+
+#include "..\..\include\Types.h"
+#include "classes.h"
+
+#define OrangeLED_ID 2
+#define RedLED_ID 5
+#define GetSignalQuality_Send_SignalID 0x12C00040
+#define GetSignalQuality_Receive_SignalID 0x12C007C0
+#define MainInput_data 0x4BA267D8
+#define MainInput_CurPos_Offset 0x8
+
+void (*DisplayGC_AddRef)(GC*,void *)=(void(*)(GC*,void *))0x143CF571;
+void (*TextObject_SetText)(IRichText * pTextObject,TEXTID strid)=(void(*)(IRichText * pTextObject,TEXTID strid))0x1416F6BD;
+void (*TextObject_SetFont)(IRichText * pTextObject,void * pFont,const int _0x8000000A,const int _0x7FFFFFF5)=(void(*)(IRichText * pTextObject,void * pFont,const int _0x8000000A,const int _0x7FFFFFF5))0x141725B9;
+int (*Illumination_LedID_SetLevel_Gradually)(int LED_ID,int level,int fade_time)=(int(*)(int LED_ID,int level,int fade_time))0x143E8C0D;
+int (*Illumination_LedID_SetLevel)(int LED_ID,int level)=(int(*)(int LED_ID,int level))0x144C7089;
+int (*Illumination_LedID_Off)(int LED_ID)=(int(*)(int LED_ID))0x14338189;
+int (*RichTextLayout_GetTextWidth)(TEXTID strid,IRichTextLayout * pRichTextLayout,IUnknown * pFont)=(int(*)(TEXTID strid,IRichTextLayout * pRichTextLayout,IUnknown * pFont))0x144221C9;
+int (*REQUEST_HPHONEBOOK_READSTRING)(const int* sync,void * buf)=(int(*)(const int* sync,void * buf))0x141ACAA1;
+void (*AB_STR_ITEM_Copy)(AB_STR_ITEM * dest,AB_STR_ITEM * source)=(void(*)(AB_STR_ITEM * dest,AB_STR_ITEM * source))0x1530F545;
+void (*Display_SetBrightnessLevel)(int level)=(void(*)(int level))0x1549C849;
+TEXTID (*TextID_CreateIntegerID_int)(int num,char * filename,int line_num)=(TEXTID(*)(int num,char * filename,int line_num))0x143D56D5;
+TEXTID (*TextID_Create_int)(const void* wstr,int flag,int len,char * filename,int line_num)=(TEXTID(*)(const void* wstr,int flag,int len,char * filename,int line_num))0x142D73ED;
+int (*TextID_GetWString_int)(TEXTID strid,wchar_t * dest,int maxlen,char * filename,int line_num)=(int(*)(TEXTID strid,wchar_t * dest,int maxlen,char * filename,int line_num))0x1416E9D9;
+int (*TextID_GetLength_int)(TEXTID strid,char * filename,int line_num)=(int(*)(TEXTID strid,char * filename,int line_num))0x1416F585;
+void (*TextID_Destroy_int)(TEXTID strid,char * filename,int line_num)=(void(*)(TEXTID strid,char * filename,int line_num))0x1416ED6D;
+TEXTID (*TextID_Copy_int)(TEXTID strid,char * filename,int line_num)=(int(*)(TEXTID strid,char * filename,int line_num))0x1416E4B5;
+int (*Request_SIM_IMSI_Read)(const int* sync,const int unk,char * imsi,char * error_cause)=(int(*)(const int* sync,const int unk,char * imsi,char * error_cause))0x100EBD2D;
+int (*get_imsi_const)()=(int(*)())0x100F62ED;
+int (*Request_SL_GetRadioState)(const int* sync,char * level,char * flag,char * is_error)=(int(*)(const int* sync,char * level,char * flag,char * is_error))0x105E1661;
+int (*Network_INetworkConnection_GetConnectionInfo)(CONNECTION_INFO_DESC * desc)=(int(*)(CONNECTION_INFO_DESC * desc))0x1423D60D;
+int (*GetChipID_int)()=(int(*)())0x1081B9D5;
+OSBOOLEAN (*get_mem_int)(int,int,void*)=(OSBOOLEAN(*)(int,int,void*))0x101E4B7C;
+int (*ConnectionManager_Connection_GetState_int)(char * buf)=(int(*)(char * buf))0x144809BD;
+void (*TextID_GetString_int)(TEXTID, char* str, int maxlen, int null)=(void(*)(TEXTID, char* str, int maxlen, int null))0x1416E6FD;
+void (*Timer_ReSet_int)(u16* timer, int time, TIMERPROC onTimer, LPARAM lparam,char * filename,int line_num)=(void(*)(u16* timer, int time, TIMERPROC onTimer, LPARAM lparam,char * filename,int line_num))0x14169D49;
+u16 (*Timer_Set_int)(int time, TIMERPROC onTimer, LPARAM lparam,char * filename,int line_num)=(u16(*)(int time, TIMERPROC onTimer, LPARAM lparam,char * filename,int line_num))0x1423D299;
+void (*Timer_Kill_int)(u16* timerID,char * filename,int line_num)=(void(*)(u16* timerID,char * filename,int line_num))0x1416A039;
---
+++ ElfPack/asm/T715_R1GA026.asm
@@ -0,0 +1,240 @@
+//T715_R1GA026
+#include "target.h"
+        RSEG   CODE
+        CODE32
+
+defadr  MACRO   a,b
+        PUBLIC  a
+a       EQU     b
+        ENDM
+
+        RSEG  CODE
+        
+        defadr   DB_PATCH1_RET,0x142D9090+1
+        defadr   DB_PATCH2_RET,0x142D9488+1
+        defadr   DB_PATCH3_RET,0x14F0009C+1
+        defadr   DB_PATCH4_RET,0x14F2888C+1
+        defadr   MESS_HOOK_RET,0x10129090+1
+
+        defadr  memalloc,0x4BA313F0
+        defadr  memfree,0x4BA31418
+
+LastExtDB equ 0x15A7E028
+
+// --- Patch Keyhandler ---
+	EXTERN Keyhandler_Hook
+	RSEG  PATCH_KEYHANDLER1
+        RSEG  CODE
+        CODE16
+NEW_KEYHANDLER1:
+	MOV	R3, R6
+	MOV	R7, R0
+	MOV	R2, #0x1
+	LDR	R1, [SP,#0]
+	MOV	R0, R4
+	BLX	Keyhandler_Hook
+	MOV	R4, R0
+	MOV	R2, R7
+	MOV	R3, #0x1
+	MOV	R1, R4
+	MOV	R0, R6
+	LDR	R7, =SFE(PATCH_KEYHANDLER1)+1
+	BX	R7
+
+	RSEG  PATCH_KEYHANDLER1
+        CODE16
+        LDR     R2,=NEW_KEYHANDLER1
+        BX      R2
+
+
+	RSEG  PATCH_KEYHANDLER2
+        RSEG  CODE
+        CODE16
+NEW_KEYHANDLER2:
+	MOV	R3, R7
+	PUSH	{R0,R1}
+	MOV	R2, R1
+	LDR	R1, [SP,#8]
+	ADD	R0, SP,#0xC
+	LDRH	R0, [R0,#0x8]
+	BLX	Keyhandler_Hook
+	POP	{R2,R3}
+	ADD	R1, SP,#0x4
+	STRH	R0, [R1,#0x8]
+	MOV	R1, R0
+	LDR	R0, =SFE(PATCH_KEYHANDLER2)+1
+	BX	R0
+
+	RSEG  PATCH_KEYHANDLER2
+        CODE16
+        LDR     R3,=NEW_KEYHANDLER2
+        BX      R3
+
+
+	RSEG  PATCH_KEYHANDLER3
+        RSEG  CODE
+        CODE16
+NEW_KEYHANDLER3:
+	MOV	R3, R7
+	PUSH	{R0,R1}
+	MOV	R2, R1
+	LDR	R1, [SP,#8]
+	LDRH	R0, [R4,#0]
+	BLX	Keyhandler_Hook
+	MOV	R2, SP
+	STRH	R0, [R2,#16]
+	MOV	R1, R0
+	MOV	R0, R7
+	LDR	R2, =SFE(PATCH_KEYHANDLER3)+1
+	MOV	R12, R2
+	POP	{R2,R3}
+	BX	R12
+
+	RSEG  PATCH_KEYHANDLER3
+        CODE16
+        LDR     R3,=NEW_KEYHANDLER3
+        BX      R3
+
+
+	RSEG  PATCH_KEYHANDLER3_NOP(1)
+        CODE16
+	NOP
+
+
+	RSEG  PATCH_KEYHANDLER3_CHANGE1(1)
+        CODE16
+	MOV	R2, SP
+	LDRH	R0, [R2,#4]
+
+
+	RSEG  PATCH_KEYHANDLER3_CHANGE2(1)
+        CODE16
+	MOV	R3, SP
+	LDRH	R0, [R3,#4]
+
+
+// --- ParseHelperMessage ---
+        EXTERN  ParseHelperMessage
+        RSEG   CODE
+        CODE16
+MESS_HOOK:
+	MOV	R7, #1
+        LDR     R5, [R6, #0]
+
+        BLX     ParseHelperMessage
+        LDR     R3, =MESS_HOOK_RET
+        BX      R3
+
+        RSEG   PATCH_MMI_MESSAGE_HOOK
+        CODE16
+        LDR     R7,=MESS_HOOK
+        BX      R7
+
+// --- PageAction1 ---
+        EXTERN  PageAction_Hook2
+        RSEG    PATCH_PageActionImpl_All
+        RSEG   CODE
+        CODE16
+PG_ACTION:
+	MOV	R2, R5
+	LDR	R1, [SP,#0x14]
+	MOV	R0, R4
+        BLX     PageAction_Hook2
+        LDR     R7,=SFE(PATCH_PageActionImpl_All)+1
+        BX      R7
+
+
+
+        RSEG    PATCH_PageActionImpl_All
+        CODE16
+        LDR     R2, =PG_ACTION
+        BX      R2
+
+
+        EXTERN  PageAction_Hook2
+        RSEG    PATCH_PageActionImpl_EnterExit
+        RSEG   CODE
+        CODE16
+PG_ACTION2:
+	LDR	R2, [SP,#0x1C]
+	LDR	R1, [SP,#0x24]
+	MOV	R0, R5
+        BLX     PageAction_Hook2
+        LDR     R7,=SFE(PATCH_PageActionImpl_EnterExit)+1
+        BX      R7
+
+
+        RSEG    PATCH_PageActionImpl_EnterExit
+        CODE16
+        LDR     R2, =PG_ACTION2
+        BX      R2
+
+// --- Data Browser ---
+
+        EXTERN  GetExtTable
+        RSEG   CODE
+        CODE16
+DB_PATCH1:
+        BLX     GetExtTable
+        LSL     R1, R4, #2
+        LDR     R0, [R0,R1]
+        LDR     R1, =LastExtDB
+        LDR     R3, =DB_PATCH1_RET
+        BX      R3
+
+
+        RSEG   CODE
+        CODE16
+DB_PATCH2:
+
+        BLX     GetExtTable
+	LSL	R1, R7, #2
+	LDR	R0, [R0,R1]
+	LDR	R1, =LastExtDB
+        LDR     R3, =DB_PATCH2_RET
+        BX      R3
+
+        RSEG   CODE
+        CODE16
+DB_PATCH3:
+        BLX     GetExtTable
+	LSL	R1, R5, #2
+	LDR	R7, [R0,R1]
+	LDR	R0, =LastExtDB
+        LDR     R3, =DB_PATCH3_RET
+        BX      R3
+
+
+        RSEG   CODE
+        CODE16
+DB_PATCH4:
+        BLX     GetExtTable
+	LSL	R1, R5, #2
+	LDR	R0, [R0,R1]
+	LDR	R1, =LastExtDB
+	STR	R0, [SP,#0]
+        LDR     R3, =DB_PATCH4_RET
+        BX      R3
+
+
+        RSEG   PATCH_DB1(2)
+        CODE16
+        LDR    R3, =DB_PATCH1
+        BX     R3
+
+        RSEG   PATCH_DB2(2)
+        CODE16
+        LDR    R3, =DB_PATCH2
+        BX     R3
+
+        RSEG   PATCH_DB3(2)
+        CODE16
+        LDR    R3, =DB_PATCH3
+        BX     R3
+
+        RSEG   PATCH_DB4(2)
+        CODE16
+        LDR    R3, =DB_PATCH4
+        BX     R3
+
+        END
